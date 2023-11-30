781c83f749591f1c205b60f06d1437d669a34c55
zazedd <leomendesantos@gmail.com>
2023-11-30 12:50:24 +0000

    probably working

--- planet/planet.cpp
+++ planet/planet.cpp
@@ -17,22 +17,6 @@
 #include "model.h"
 #include "shader.h"
 
-#include "imgui.h"
-#include "imgui_impl_glfw.h"
-#include "imgui_impl_opengl3.h"
-
-// Setup Dear ImGui context
-IMGUI_CHECKVERSION();
-ImGui::CreateContext();
-ImGuiIO& io = ImGui::GetIO();
-io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
-io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
-io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;         // IF using Docking Branch
-
-// Setup Platform/Renderer backends
-ImGui_ImplGlfw_InitForOpenGL(YOUR_WINDOW, true);          // Second param install_callback=true will install GLFW callbacks and chain to existing ones.
-ImGui_ImplOpenGL3_Init();
-
 const GLint WIDTH = 1400, HEIGHT = 800;
 const double PI = 3.141592653589793238463;
 int SCREEN_WIDTH, SCREEN_HEIGHT;
@@ -184,14 +168,6 @@ int system() {
   glEnable(GL_CULL_FACE);
   glCullFace(GL_BACK);
   glfwSwapInterval(0);
-
-  // (Your code calls glfwPollEvents())
-  // ...
-  // Start the Dear ImGui frame
-  ImGui_ImplOpenGL3_NewFrame();
-  ImGui_ImplGlfw_NewFrame();
-  ImGui::NewFrame();
-  ImGui::ShowDemoWindow(); // Show demo window! :)
 
   Shader shader("resources/shaders/modelLoading.vs",
                 "resources/shaders/modelLoading.frag");
